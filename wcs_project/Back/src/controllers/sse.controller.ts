2283f8430d16ba99e69264892cee9eab640d6146
ShintaroHiro <94305927+ShintaroHiro@users.noreply.github.com>
2026-01-16 21:32:44 +0700

    Update sse.controller.ts

--- wcs_project/Back/src/controllers/sse.controller.ts
+++ wcs_project/Back/src/controllers/sse.controller.ts
@@ -16,6 +16,7 @@ export const connectSSE = (req: Request, res: Response) => {
     res.setHeader("Content-Type", "text/event-stream");
     res.setHeader("Cache-Control", "no-cache");
     res.setHeader("Connection", "keep-alive");
+    res.setHeader("X-Accel-Buffering", "no");
     res.flushHeaders();
 
     addClient(counterId, res);
